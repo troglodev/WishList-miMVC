ac25a21a8ffbed8cc4d31e0f5c0399b10612cd16
Troglodyte Developer <troglodev@gmail.com>
2012-04-28 00:21:27 +0200

    Eliminando hardcodeo

--- config/config.php
+++ config/config.php
@@ -10,84 +10,83 @@ $array_classes = array('libs/SPDO.php',
 
 
 //******RUTAS
-        const CONFIG_BD_INI = 'config/bd.ini';
+//Ini Parametros BBDD
+define('CONFIG_BD_INI', 'config/bd.ini');
+
+
+//RUTAS MVC
+define('BOOTSTRAP', 'index.php?');
+define('RUTA_MODELOS', 'models/');
+define('RUTA_VISTAS', 'views/');
+define('RUTA_CONTROLADORES', 'controllers/');
+define('NOMBRE_CONTROLADOR', 'controlador=');
+define('NOMBRE_ACCION', '&accion=');
+
+
+//INC de HTML
+define('RUTA_HTML', 'views/html/');
+define('HTML_ACCESO', RUTA_HTML . 'acceso.inc.php');
+define('HTML_DERECHA', RUTA_HTML . 'derecha.inc.php');
+define('HTML_HEAD', RUTA_HTML . 'head.inc.php');
+define('HTML_PIE', RUTA_HTML . 'pie.inc.php');
+
+
+//ACTIONS DE LOS FORMULARIOS
+define('ACTION_USUARIO_NUEVO', BOOTSTRAP . NOMBRE_CONTROLADOR . 'usuario' . NOMBRE_ACCION . 'registrar');
+define('ACTION_USUARIO_MOSTRARFORM', BOOTSTRAP . NOMBRE_CONTROLADOR . 'usuario' . NOMBRE_ACCION . 'mostrarFormulario');
+define('ACTION_USUARIO_DESCONECTAR', BOOTSTRAP . NOMBRE_CONTROLADOR . 'usuario' . NOMBRE_ACCION . 'desconectar');
+define('ACTION_USUARIO_ACCEDER', BOOTSTRAP . NOMBRE_CONTROLADOR . 'usuario' . NOMBRE_ACCION . 'acceder');
+
+define('ACTION_DESEO_GUARDAR', BOOTSTRAP . NOMBRE_CONTROLADOR . 'deseo' . NOMBRE_ACCION . 'guardar');
+define('ACTION_DESEO_GUARDAR_ID', BOOTSTRAP . NOMBRE_CONTROLADOR . 'deseo' . NOMBRE_ACCION.'guardar&id=');
+define('ACTION_DESEO_MOSTRAR', BOOTSTRAP . NOMBRE_CONTROLADOR . 'deseo' . NOMBRE_ACCION.'mostrar');
+define('ACTION_DESEO_MOSTRARCUMPLIDOS', BOOTSTRAP . NOMBRE_CONTROLADOR . 'deseo' . NOMBRE_ACCION . 'mostrarCumplidos');
+define('ACTION_DESEO_NUEVO', BOOTSTRAP . NOMBRE_CONTROLADOR . 'deseo' . NOMBRE_ACCION . 'nuevo');
+define('ACTION_DESEO_EDITAR_ID', BOOTSTRAP . NOMBRE_CONTROLADOR . 'deseo' . NOMBRE_ACCION . 'editar&id=');
+define('ACTION_DESEO_ELIMINAR_ID', BOOTSTRAP . NOMBRE_CONTROLADOR . 'deseo' . NOMBRE_ACCION . 'eliminar&id=');
+define('ACTION_DESEO_CUMPLIR_ID', BOOTSTRAP . NOMBRE_CONTROLADOR . 'deseo' . NOMBRE_ACCION . 'cumplir&id=');
+
+
+//MODELOS
+define('MODELO_DESEO', 'DeseoModel.php');
+define('MODELO_USUARIO', 'UsuarioModel.php');
+
+
+//VISTAS
+define('VISTA_USUARIO_REGISTRAR', 'usuario/usuarioRegistrar.php');
+DEFINE('VISTA_INICIO', 'default/index.php');
+
+//CONTROLADORES
+define('CONTROLADOR_DESEO', 'deseoController.php');
+define('CONTROLADOR_INICIO', 'inicioController.php');
+define('CONTROLADOR_USUARIO', 'usuarioController.php');
+
+
+
+//ACCIONES DE LOS CONTROLADORES
+define('ACCION_NUEVO_DESEO', 'nuevoDeseo');
+define('ACCION_MOSTRAR_DESEO', 'mostrarDeseo');
+define('ACCION_MOSTRAR_CUMPLIDOS', 'mostrarCumplidos');
+define('ACCION_EDITAR_DESEO', 'editarDeseo');
+define('ACCION_MODIFICAR_DESEO', 'modificarDeseo');
+define('ACCION_CUMPLIR_DESEO', 'cumplirDeseo');
+define('ACCION_INSERTAR_DESEO', 'insertarDeseo');
+define('ACCION_ELIMINAR_DESEO', 'eliminarDeseo');
 
 
 
 
+define('CONTROLADOR_PREDEFINIDO', "inicio");
+define('ACCION_PREDEFINIDA', "mostrarInicio");
 
+//define('ruta_inc','inc/');
+define('RUTA_DBH', 'core/bd.php');
 
-        const RUTA_MODELOS = 'models/';
-        const RUTA_CONTROLADORES = 'controllers/';
-        const RUTA_INC = 'views/html/';
-        const BOOTSTRAP = 'index.php?';
-        const URL_CONTROLADOR = 'controlador=';
-        const URL_ACCION = '&accion=';
-        const URL_INICIO = 'default/index.php';
-
-        const ACTION_USUARIO_NUEVO = 'index.php?controlador=usuario&accion=registrar';
-        const ACTION_USUARIO_MOSTRARFORM = 'index.php?controlador=usuario&accion=mostrarFormulario';
-        const ACTION_USUARIO_DESCONECTAR = 'index.php?controlador=usuario&accion=desconectar';
-        const ACTION_USUARIO_ACCEDER = 'index.php?controlador=usuario&accion=acceder';
-        const ACTION_DESEO_GUARDAR = 'index.php?controlador=deseo&accion=guardar';
-        const ACTION_DESEO_GUARDAR_ID = 'index.php?controlador=deseo&accion=guardar&id=';
-
-        const ACTION_DESEO_MOSTRAR = 'index.php?controlador=deseo&accion=mostrar';
-        const ACTION_DESEO_MOSTRARCUMPLIDOS = 'index.php?controlador=deseo&accion=mostrarCumplidos';
-        const ACTION_DESEO_NUEVO = 'index.php?controlador=deseo&accion=nuevo';
-
-
-        const ACTION_DESEO_EDITAR_ID = 'index.php?controlador=deseo&accion=editar&id=';
-        const ACTION_DESEO_ELIMINAR_ID = 'index.php?controlador=deseo&accion=eliminar&id=';
-        const ACTION_DESEO_CUMPLIR_ID = 'index.php?controlador=deseo&accion=cumplir&id=';
-
-//******INC de HTML
-        const HTML_ACCESO = 'acceso.inc.php';
-        const HTML_DERECHA = 'derecha.inc.php';
-        const HTML_HEAD = 'head.inc.php';
-        const HTML_PIE = 'pie.inc.php';
-
-//******VISTAS
-        const VISTA_USUARIO_REGISTRAR = 'usuario/usuarioRegistrar.php';
-
-
-
-
-
-//******MODELOS
-        const MODELO_DESEO = 'DeseoModel.php';
-        const MODELO_USUARIO = 'UsuarioModel.php';
-
-//******ACCIONES
-
-
-        const ACCION_NUEVO_DESEO = 'nuevoDeseo';
-        const ACCION_MOSTRAR_DESEO = 'mostrarDeseo';
-        const ACCION_MOSTRAR_CUMPLIDOS = 'mostrarCumplidos';
-        const ACCION_EDITAR_DESEO = 'editarDeseo';
-        const ACCION_MODIFICAR_DESEO = 'modificarDeseo';
-        const ACCION_CUMPLIR_DESEO = 'cumplirDeseo';
-        const ACCION_INSERTAR_DESEO = 'insertarDeseo';
-        const ACCION_ELIMINAR_DESEO = 'eliminarDeseo';
-
-//******CONTROLADORES
-        const CONTROLADOR_DESEO = 'deseoController.php';
-        const CONTROLADOR_INICIO = 'inicioController.php';
-        const CONTROLADOR_USUARIO = 'usuarioController.php';
-
-
-
-        const CONTROLADOR_PREDEFINIDO = "inicio";
-        const ACCION_PREDEFINIDA = "mostrarInicio";
-
-//const ruta_inc = 'inc/';
-        const RUTA_DBH = 'core/bd.php';
-        const RUTA_VISTAS = 'views/default/';
-        const RUTA_IMAGENES = 'views/estatico/img/';
+define('RUTA_IMAGENES', 'views/estatico/img/');
 
 /*
   function getConfig($seccion) {
-  $data = parse_ini_file('../config/config.ini', true);
+  $data,parse_ini_file('../config/config.ini', true);
   return $data[$seccion];
   }
  */
